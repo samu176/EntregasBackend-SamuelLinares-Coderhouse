a3c01f5b0602c49613599e8c909d55c8a71dce9e
Samuel Linares <samu.f167@gmail.com>
2024-03-20 20:49:05 -0300

    Mailing y mensajería y mocking

--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -1,21 +1,18 @@
 const errorMessages = {
-    'ValidationError': 'Datos de entrada inválidos',
-    'CastError': 'Formato de ID inválido',
-    'MongoError': 'Error de base de datos',
-    'ProductNotFound': 'Producto no encontrado',
-    'UserNotFound': 'Usuario no encontrado',
-    'InvalidCredentials': 'Credenciales inválidas',
-    'EmailAlreadyExists': 'El correo electrónico ya está en uso',
-  };
-  
-  function errorHandler(err, req, res, next) {
+    'AddProductError': 'Error al agregar un producto a la base de datos',
+    'AddToCartError': 'Error al agregar un producto al carrito',
+    'CreateCartError': 'Error al crear un carrito',
+    'EmptyCartError': 'Error al vaciar el carrito',
+};
+
+function errorHandler(err, req, res, next) {
     const statusCode = err.statusCode || 500;
     const message = errorMessages[err.name] || err.message || 'Error interno del servidor';
     res.status(statusCode).json({
-      status: 'error',
-      statusCode: statusCode,
-      message: message
+        status: 'error',
+        statusCode: statusCode,
+        message: message
     });
-  }
-  
-  module.exports = errorHandler;
+}
+
+module.exports = errorHandler;
